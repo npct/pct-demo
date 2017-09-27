43bd8f2dd79efe5a4d4ba55d1109fa54736059a1
Robin Lovelace <rob00x@gmail.com>
2017-09-27 18:23:24 +0100

    Add demo shiny app to show how shiny works
    Heads up @Annagoodman1 - anything else we should put in there?

---
+++ app.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(leaflet)
+library(sf)
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
+
+ui <- fluidPage(
+  leafletOutput("pctShinyMap"),
+  p(),
+  actionButton("recalc", "New lines")
+)
+
+server <- function(input, output, session) {
+  
+  source("code/load-data.R")
+  
+  lines <- eventReactive(input$recalc, {
+    rf[sample(x = nrow(rf), size = 10),]
+  }, ignoreNULL = FALSE)
+  
+  output$pctShinyMap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.TonerLite,
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>%
+      addPolylines(data = lines())
+  })
+}
+
+shinyApp(ui, server)
