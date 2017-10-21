fc4ab77d134d7a30c16cbe12d1cf9c12eca2b876
Robin Lovelace <rob00x@gmail.com>
2017-10-21 17:15:30 +0100

    Add data, use sp for bookdown.org

--- shortCarJourneys/server.R
+++ shortCarJourneys/server.R
@@ -1,14 +1,18 @@
 library(leaflet.extras)
+library(leaflet)
+library(shiny)
 library(dplyr)
+library(sp)
 function(input, output, session) {
   
   source("load-data.R")
   
   lines <- reactive({
-    rf %>% 
-      top_n(n = input$nos_lines, wt = car_driver) %>% 
-      mutate(pcycle = bicycle / all * 100)
-    
+    rf$pcycle = rf$bicycle / rf$all
+    rf = rf[rf$rf_dist_km < input$max_dist, ]
+    top_n_lines = head(sort(rf$car_driver, decreasing = TRUE, index.return = TRUE)$ix, n = input$nos_lines)
+    rf = rf[unique(top_n_lines), ]
+    rf
   })
   
   output$pctShinyMap <- renderLeaflet({
@@ -21,4 +25,5 @@ function(input, output, session) {
       addFullscreenControl() %>% 
       addLegend("bottomright", pal = pal, values = lines()$pcycle, title = "Percent cycling")
   })
+  print(nrow(rf))
 }
--- shortCarJourneys/ui.R
+++ shortCarJourneys/ui.R
@@ -1,13 +1,20 @@
+library(leaflet)
 fluidPage(
-  leafletOutput("pctShinyMap"),
+  leafletOutput("pctShinyMap", height = 300),
   p(),
   actionButton("recalc", "New lines"),
   sliderInput(
     "nos_lines",
-    label = "Top N Lines (most cycled)",
+    label = "Top N Lines (number of short car journeys)",
     1,
     200,
-    value = 30,
-    ticks = F
-  )
+    value = 50
+    ),
+  sliderInput(
+    "max_dist",
+    label = "Maximum distance (km)",
+    0,
+    5,
+    value = 5
+    )
 )
